ca2208054231c87741d023dc97b001fdf5e77f13
Denis Zhurov <den_fobos@mail.ru>
2021-11-01 16:33:34 +0300

    fixed header

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import Dropdown from "../dropdown/Dropdown";
+import Navlink from "../navlinkbutton/NavlinkButton";
+
+export default function Navbar(): JSX.Element {
+  return (
+    <nav className="navigation">
+      <ul>
+        <Navlink text="Home" className="active" link="#" />
+        <Dropdown text="Products" />
+        <Navlink text="About" />
+        <Navlink text="Sign In" />
+        <Navlink text="Sign Up" />
+      </ul>
+    </nav>
+  );
+}
